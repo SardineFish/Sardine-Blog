4c267d8039dc4605b98da9a3aa00b159b8f4009d
SardineFish <Sardine@live.cn>
2020-12-10 19:08:00 +0800

    Add steam stat query api

---
+++ api/stat/steam_stat.php
@@ -0,0 +1,21 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT']."/lib/mysql/const.php";
+$url = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=".STEAM_API_KEY."&steamid=".STEAM_ID."&format=json";
+
+$context  = stream_context_create(array());
+$response = file_get_contents($url, false, $context);
+$stat = json_decode($response);
+$stat = $stat->response;
+$count = (int)$stat->game_count;
+
+for ($i = 0; $i < $count; $i ++)
+{
+    if ($stat->games[$i]->playtime_forever <= 0)
+        continue;
+    $url = "http://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2/?key=".STEAM_API_KEY."&appid=".$stat->games[$i]->appid;
+    $response = file_get_contents($url, false, $context);
+    $stat->games[$i]->game_name = $response->game->gameName;
+}
+
+echo(json_encode($stat));
+?>
