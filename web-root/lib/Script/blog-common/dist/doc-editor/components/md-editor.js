e0dc3b6fa6933736373bd748f3639d81a8ca1449
SardineFish <Sardine@live.cn>
2022-10-31 22:56:54 +0800

    Improve blog editor markdown
    Signed-off-by: SardineFish <Sardine@live.cn>

--- web-root/lib/Script/blog-common/dist/doc-editor/components/md-editor.js
+++ web-root/lib/Script/blog-common/dist/doc-editor/components/md-editor.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useRef, useState } from "react";
 import SimpleMDE from "simplemde";
-import { marked } from "marked";
 import { SelectGroup } from "../../component";
 import { error } from "../../misc";
-window.marked = marked;
+import markdownit from "markdown-it";
+import markdownitEmoji from "markdown-it-emoji";
+// @ts-ignore
+import markdownitKatex from "@iktakahiro/markdown-it-katex";
+import katex from "katex";
+const markdownitKatexWrap = (md, option) => markdownitKatex(md, option, katex);
 var EditMode;
 (function (EditMode) {
     EditMode["Preview"] = "preview";
@@ -19,11 +23,35 @@ export function MarkdownEditor(props) {
         if (!ref.current)
             return;
         if (!editor) {
+            const md = markdownit({
+                html: true,
+                xhtmlOut: false,
+                breaks: true,
+                langPrefix: 'lang-',
+                linkify: true,
+                typographer: false,
+            });
+            md.use(markdownitEmoji);
+            md.use(markdownitKatexWrap);
+            md.use(mdImgPostprocess);
+            let delayTimeout = -1;
             const editor = new SimpleMDE({
                 element: ref.current,
                 initialValue: "",
                 toolbar: false,
                 spellChecker: false,
+                previewRender: (text, preview) => {
+                    if (preview) {
+                        clearTimeout(delayTimeout);
+                        delayTimeout = setTimeout(() => {
+                            // previousRenderResult = md.render(text);
+                            preview.innerHTML = md.render(text);
+                        }, 100);
+                        return "rendering...";
+                    }
+                    else
+                        return md.render(text);
+                },
             });
             setTimeout(() => {
                 editor.toggleSideBySide();
@@ -48,5 +76,68 @@ export function MarkdownEditor(props) {
                 React.createElement(SelectGroup.Item, { id: EditMode.Markdown }, "Markdown"))),
         React.createElement("main", { className: "md-input markdown-body" },
             React.createElement("textarea", { ref: ref }))));
+}
+function imagePostProcess(imgUrl) {
+    const src = imgUrl;
+    imgUrl = SardineFish.API.Storage.processImg(imgUrl, "Size600");
+    console.log(`${src} -> ${imgUrl}`);
+    return imgUrl;
+}
+function mdImgPostprocess(md) {
+    const imgProcess = md.renderer.rules.image;
+    const htmlBlockProcess = md.renderer.rules.html_block;
+    const htmlInlineProcess = md.renderer.rules.html_inline;
+    md.renderer.rules.image = function (tokens, idx, options, env, slf) {
+        var token = tokens[idx];
+        if (token.attrs) {
+            var imgUrl = token.attrs[token.attrIndex('src')][1];
+            imgUrl = imagePostProcess(imgUrl);
+            //imgUrl = defaultImageHost.replace(webArchivePrefixReplacer, imgUrl);
+            console.log(`${token.attrs[token.attrIndex('src')][1]} -> ${imgUrl}`);
+            token.attrs[token.attrIndex('src')][1] = imgUrl;
+        }
+        let result = imgProcess(tokens, idx, options, env, slf);
+        if (token.attrGet("alt")) {
+            result += `<aside class="img-note">${token.attrGet("alt")}</aside>`;
+        }
+        return result;
+    };
+    const handleHTML = (token) => {
+        const html = token.content;
+        const wrapper = document.createElement("div");
+        wrapper.innerHTML = html;
+        for (const img of Array.from(wrapper.querySelectorAll("img"))) {
+            img.src = imagePostProcess(img.src);
+        }
+        createImgNoteForImgGroup(wrapper);
+        // console.log(wrapper.innerHTML);
+        token.content = wrapper.innerHTML;
+    };
+    md.renderer.rules.html_block = (tokens, idx, options, env, slf) => {
+        var token = tokens[idx];
+        handleHTML(token);
+        return htmlBlockProcess(tokens, idx, options, env, slf);
+    };
+    md.renderer.rules.html_inline = (tokens, idx, options, env, slf) => {
+        var token = tokens[idx];
+        handleHTML(token);
+        return htmlInlineProcess(tokens, idx, options, env, slf);
+    };
+}
+function createImgNoteForImgGroup(wrapper) {
+    const group = wrapper.querySelector(".img-row");
+    const imgs = group.querySelectorAll("img");
+    if (imgs.length === 0)
+        return;
+    const notes = Array.from(imgs).map(img => {
+        const element = document.createElement("aside");
+        element.innerText = img.alt;
+        element.classList.add("img-note");
+        return element;
+    });
+    const noteElement = document.createElement("aside");
+    noteElement.classList.add("img-row-note");
+    notes.forEach(node => noteElement.appendChild(node));
+    group.insertAdjacentElement("afterend", noteElement);
 }
 //# sourceMappingURL=md-editor.js.map
--- web-root/lib/Script/blog-common/src/doc-editor/components/md-editor.tsx
+++ web-root/lib/Script/blog-common/src/doc-editor/components/md-editor.tsx
@@ -1,10 +1,17 @@
 import React, { MutableRefObject, useEffect, useRef, useState } from "react";
 import SimpleMDE from "simplemde";
-import { marked } from "marked";
 import { SelectGroup } from "../../component";
 import { error } from "../../misc";
+import markdownit from "markdown-it";
+import markdownitEmoji from "markdown-it-emoji";
+// @ts-ignore
+import markdownitKatex from "@iktakahiro/markdown-it-katex";
+import katex from "katex";
+import MarkdownIt from "markdown-it";
+import { RenderRule } from "markdown-it/lib/renderer";
+import Token from "markdown-it/lib/token";
 
-(window as any).marked = marked;
+const markdownitKatexWrap = (md: any, option: any) => markdownitKatex(md, option, katex);
 
 export interface MarkdownEditorRef {
     getDoc(): string,
@@ -34,11 +41,41 @@ export function MarkdownEditor(props: {handle?: MutableRefObject<MarkdownEditorR
         
         if (!editor)
         {
+            const md = markdownit({
+                html: true, // Enable HTML tags in source
+                xhtmlOut: false, // Use '/' to close single tags (<br />).
+                breaks: true, // Convert '\n' in paragraphs into <br>
+                langPrefix: 'lang-', // CSS language prefix for fenced blocks. Can be
+                linkify: true, // Autoconvert URL-like text to links
+                typographer: false,
+            });
+
+            md.use(markdownitEmoji);
+            md.use(markdownitKatexWrap);
+            md.use(mdImgPostprocess);
+
+            let delayTimeout = -1;
             const editor = new SimpleMDE({
                 element: ref.current,
                 initialValue: "",
                 toolbar: false,
                 spellChecker: false,
+                previewRender: (text, preview) =>
+                {
+                    if (preview)
+                    {
+                        clearTimeout(delayTimeout);
+                        delayTimeout = setTimeout(() =>
+                        {
+                            // previousRenderResult = md.render(text);
+                            preview.innerHTML = md.render(text);
+                        }, 100);
+                        return "rendering...";
+                    }
+                    else 
+                        return md.render(text);
+                },
+
             });
             setTimeout(() =>
             {
@@ -73,4 +110,90 @@ export function MarkdownEditor(props: {handle?: MutableRefObject<MarkdownEditorR
             <textarea ref={ref}></textarea>
         </main>
     </div>)
+}
+
+function imagePostProcess(imgUrl: string)
+{
+    const src = imgUrl;
+
+    imgUrl = SardineFish.API.Storage.processImg(imgUrl, "Size600");
+
+    console.log(`${src} -> ${imgUrl}`);
+
+    return imgUrl;
+}
+
+function mdImgPostprocess(md: MarkdownIt)
+{
+    const imgProcess = md.renderer.rules.image;
+    const htmlBlockProcess = md.renderer.rules.html_block;
+    const htmlInlineProcess = md.renderer.rules.html_inline;
+
+    md.renderer.rules.image = function (tokens, idx, options, env, slf)
+    {
+        var token = tokens[idx];
+        if (token.attrs)
+        {
+            var imgUrl = token.attrs[token.attrIndex('src')][1];
+            imgUrl = imagePostProcess(imgUrl);
+
+            //imgUrl = defaultImageHost.replace(webArchivePrefixReplacer, imgUrl);
+            console.log(`${token.attrs[token.attrIndex('src')][1]} -> ${imgUrl}`);
+            token.attrs[token.attrIndex('src')][1] = imgUrl;
+        }
+        let result = (imgProcess as RenderRule)(tokens, idx, options, env, slf) 
+        if (token.attrGet("alt"))
+        {
+            result += `<aside class="img-note">${token.attrGet("alt")}</aside>`;
+        }
+
+        return result;
+    };
+
+    const handleHTML = (token: Token) =>
+    {
+        const html = token.content;
+        const wrapper = document.createElement("div");
+        wrapper.innerHTML = html;
+        for (const img of Array.from(wrapper.querySelectorAll("img")))
+        {
+            img.src = imagePostProcess(img.src);
+        }
+        createImgNoteForImgGroup(wrapper);
+        // console.log(wrapper.innerHTML);
+        token.content = wrapper.innerHTML;
+    }
+
+    md.renderer.rules.html_block = (tokens, idx, options, env, slf) =>
+    {
+        var token = tokens[idx];
+        handleHTML(token);
+        return (htmlBlockProcess as RenderRule)(tokens, idx, options, env, slf);
+    };
+
+    md.renderer.rules.html_inline = (tokens, idx, options, env, slf) =>
+    {
+        var token = tokens[idx];
+        handleHTML(token);
+        return (htmlInlineProcess as RenderRule)(tokens, idx, options, env, slf);
+    }
+}
+
+function createImgNoteForImgGroup(wrapper: HTMLDivElement)
+{
+    const group = wrapper.querySelector(".img-row") as HTMLDivElement;
+    const imgs = group.querySelectorAll("img");
+    if (imgs.length === 0)
+        return;
+    const notes = Array.from(imgs).map(img =>
+    {
+        const element = document.createElement("aside");
+        element.innerText = img.alt;
+        element.classList.add("img-note");
+        return element;
+    });
+    const noteElement = document.createElement("aside");
+    noteElement.classList.add("img-row-note");
+    notes.forEach(node => noteElement.appendChild(node));
+    group.insertAdjacentElement("afterend", noteElement);
 }
